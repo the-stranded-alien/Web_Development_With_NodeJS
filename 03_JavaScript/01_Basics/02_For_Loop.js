24182af751ed86dbd3bab61cd66c96a93eb079fa
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-07-08 12:35:35 +0530

    JS - Basics

---
+++ 03_JavaScript/01_Basics/02_For_Loop.js
@@ -0,0 +1,16 @@
+let a = 10;
+
+for(let i = 0; i < a; i++) {
+    console.log(i);
+}
+
+// Print All Elements In A Single Line
+
+let b = 10;
+let result = "";
+
+for(let j = 0; j < b; j++) {
+    result += j + " ";
+}
+
+console.log(result)
