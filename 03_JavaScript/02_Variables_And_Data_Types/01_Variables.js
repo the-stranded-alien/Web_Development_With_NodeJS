6d47ea16e73f165fb0f2f72619a828119fdf9e32
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-07-08 12:35:56 +0530

    JS - Variables & Datatypes

---
+++ 03_JavaScript/02_Variables_And_Data_Types/01_Variables.js
@@ -0,0 +1,11 @@
+var a = 10 // Omit Semicolon If You Prefer
+var b = 20; // Write Semicolon If You Prefer
+
+let c = 30;
+const d = 40;
+
+c++;
+console.log(c)
+
+// d++; // Not Possible As 'd' Is Constant
+console.log(d)
---
+++ 03_JavaScript/02_Variables_And_Data_Types/02_Data_Types.js
@@ -0,0 +1,29 @@
+// Variable Is Like A Container.
+// Data Is What We Put Inside It.
+// Data Always Have Type.
+
+let num = 1;
+let str = "this is a string";
+let bool = true;
+
+// No Way To Specify Data-Type
+console.log(num)
+num = "hello";
+console.log(num)
+
+console.log(typeof true)
+console.log(typeof str)
+console.log(typeof num)
+num = 5
+console.log(typeof num)
+
+console.log(typeof null)
+console.log(typeof undefined)
+
+console.log(typeof [1,2,3])
+console.log(typeof "c")
+console.log(typeof 'c')
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_SAFE_INTEGER)
